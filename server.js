7cab35306e7eeec4e25058b3d5f0b19402bb6315
poorani11 <poorani.mgr@gmail.com>
2018-06-08 14:35:08 +0530

    update middleware for localhost

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ var express = require('express');
 
 //Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(function (req, res, next){
+    if(req.headers['x-fowarded-proto'] === 'https') {
+        res.redirect('http://' + req.hostname + req.url);
+    } else {
+         next(); 
+    }
+});
 app.use(express.static('public'));
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
     console.log('Express server is up on port 3000');
 });
