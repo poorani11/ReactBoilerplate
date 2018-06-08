a46e911de49920b1372e3eb9f66fbb6cda4a013d
poorani11 <poorani.mgr@gmail.com>
2018-06-08 22:59:14 +0530

    Intial commit

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,15 +1,9 @@
 var React = require('react');
-var Nav = require('Nav');
 
 var Main = (props) => {
     return (
         <div>
-          <Nav/>
-          <div className="row">
-              <div className="columns medium-6 large-4 small-centered">
-                  {props.children}
-              </div>
-          </div>
+          <h1 className="text-center">Main.jsx rendered</h1>
         </div>
     )
 };
--- webpack.config.js
+++ webpack.config.js
@@ -22,14 +22,6 @@ module.exports = {
             root:__dirname,
             alias: {
                 Main: 'app/components/Main.jsx',
-                Nav: 'app/components/Nav.jsx',
-                Weather: 'app/components/Weather.jsx',
-                WeatherMessage: 'app/components/WeatherMessage.jsx',
-                WeatherForm: 'app/components/WeatherForm.jsx',
-                About: 'app/components/About.jsx',
-                Examples: 'app/components/Examples.jsx',
-                openWeatherMap: 'app/api/openWeatherMap.jsx',
-                ErrorModal: 'app/components/ErrorModal.jsx',
                 applicationStyles: 'app/styles/app.scss'
             },
             extensions: ['', '.js', '.jsx']
